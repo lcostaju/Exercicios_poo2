529902b1baa6b8f9318a0a10d1a376dfa7714a2e
lcostaju <luciocj_eng@outlook.com>
2025-04-09 19:29:09 -0300

    refactor App class to improve output formatting and structure for geometric shape activities

--- atv_class_abst_polimorfismo/src/App.java
+++ atv_class_abst_polimorfismo/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
+        System.out.println("Atividade 1");
         GeometriaPlana fig = new GeometriaPlana();
         fig.mudaParaCirculo();
         
@@ -12,6 +14,19 @@ public class App {
         fig.mudaParaTriangulo();
 
         System.out.println(fig.exibe());
-        
-    }
+
+        System.out.println("Atividade 2");
+
+        Figura2D fig1[] = new Figura2D[3];
+
+        fig1[0] = new Circulo();
+        fig1[1] = new Quadrado();
+        fig1[2] = new Triangulo();
+
+        for (Figura2D figura2d : fig1) {
+            System.out.println(figura2d.pegaTexto());
+        }
+    
+        };
+
 }
